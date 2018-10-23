9b830637a0768f21e888f483c75720811815fc9e
Caleb Ehrich <cehrich02@gmail.com>
2018-10-23 15:08:48 -0400

    Added Metadata and Partial Classes for validation

--- ContosoUniversityData/Student.sql
+++ ContosoUniversityData/Student.sql
@@ -3,6 +3,7 @@
 	[StudentID] int identity(1,1) not null,
 	[LastName] nvarchar(50) null,
 	[FirstName] nvarchar(50) null,
+	[MiddleName] nvarchar(50) null,
 	[EnrollmentDate] datetime null,
 	Primary key clustered ([StudentID] asc)
 )
